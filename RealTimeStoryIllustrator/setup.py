892658b31e05c9884ce01ab2778579dc64ae324b
Konstantin Höbart <konzi.h@gmail.com>
2015-09-21 21:45:11 +0200

    Updated build

--- RealTimeStoryIllustrator/setup.py
+++ RealTimeStoryIllustrator/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages, Command
  
 setup( 
     name='RealTimeStoryIllustrator', 
-    version='0.1', 
+    version='0.2',
     packages=find_packages(), 
     include_package_data=True, 
     package_data={'rtsi.data': ['*.txt','*.png','*.jpg']}, 
